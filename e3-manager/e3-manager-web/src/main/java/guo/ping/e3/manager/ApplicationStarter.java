7807b3a9ad4e00fc6779db5e941ea7ab98cf28d6
DESKTOP-G1DGSJ4\Guopin <Kingdompin@163.com>
2018-08-03 15:55:12 +0800

    搭建工程

---
+++ e3-manager/e3-manager-web/src/main/java/guo/ping/e3/manager/ApplicationStarter.java
@@ -0,0 +1,12 @@
+package guo.ping.e3.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+}
---
+++ e3-manager/e3-manager-web/src/main/java/guo/ping/e3/manager/controller/HelloController.java
@@ -0,0 +1,15 @@
+package guo.ping.e3.manager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/index")
+    public String hello() {
+        return "index";
+    }
+}
