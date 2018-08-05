fb457b625f81aee34d3af9f049abc6eb8f26526d
DESKTOP-G1DGSJ4\Guopin <Kingdompin@163.com>
2018-08-05 10:16:46 +0800

    添加所有数据库pojo类

---
+++ e3-manager/e3-manager-pojo/src/main/java/guo/ping/e3mall/pojo/TbItemParamAndName.java
@@ -0,0 +1,14 @@
+package guo.ping.e3mall.pojo;
+
+public class TbItemParamAndName extends TbItemParam {
+
+    private String itemCatName;
+
+    public String getItemCatName() {
+        return itemCatName;
+    }
+
+    public void setItemCatName(String itemCatName) {
+        this.itemCatName = itemCatName;
+    }
+}
