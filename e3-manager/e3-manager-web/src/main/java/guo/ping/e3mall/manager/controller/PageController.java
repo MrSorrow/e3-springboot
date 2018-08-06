1429b085c94af31b56870ee95027a83a7498f6ae
DESKTOP-G1DGSJ4\Guopin <Kingdompin@163.com>
2018-08-06 17:10:44 +0800

    添加所有的后台管理页面模板

---
+++ e3-manager/e3-manager-web/src/main/java/guo/ping/e3mall/manager/controller/PageController.java
@@ -0,0 +1,15 @@
+package guo.ping.e3mall.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/{page}")
+    public String showPage(@PathVariable String page) {
+        return page;
+    }
+
+}
