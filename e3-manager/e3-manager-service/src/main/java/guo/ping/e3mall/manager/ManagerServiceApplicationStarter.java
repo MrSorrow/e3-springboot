b8ff804dc788b6a3af970dea279fd846c4a2821d
DESKTOP-G1DGSJ4\Guopin <Kingdompin@163.com>
2018-08-09 10:56:28 +0800

    不使用tomcat发布dubbo服务

--- e3-manager/e3-manager-service/src/main/java/guo/ping/e3mall/manager/ManagerServiceApplicationStarter.java
+++ e3-manager/e3-manager-service/src/main/java/guo/ping/e3mall/manager/ManagerServiceApplicationStarter.java
@@ -1,14 +1,22 @@
 package guo.ping.e3mall.manager;
 
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.concurrent.CountDownLatch;
 
 @MapperScan(value = "guo.ping.e3mall.manager.mapper")
 @SpringBootApplication
 public class ManagerServiceApplicationStarter {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ManagerServiceApplicationStarter.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        new SpringApplicationBuilder()
+                .sources(ManagerServiceApplicationStarter.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await();
     }
 }
