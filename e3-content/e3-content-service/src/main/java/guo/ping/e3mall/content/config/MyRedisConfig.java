04329d2601d9947fba6c35f85249211a53c8835e
DESKTOP-G1DGSJ4\Guopin <Kingdompin@163.com>
2018-08-10 22:06:58 +0800

    使用单击版redis

--- e3-content/e3-content-service/src/main/java/guo/ping/e3mall/content/config/MyRedisConfig.java
+++ e3-content/e3-content-service/src/main/java/guo/ping/e3mall/content/config/MyRedisConfig.java
@@ -2,13 +2,10 @@ package guo.ping.e3mall.content.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import javax.annotation.Resource;
 
 @Configuration
 public class MyRedisConfig {
