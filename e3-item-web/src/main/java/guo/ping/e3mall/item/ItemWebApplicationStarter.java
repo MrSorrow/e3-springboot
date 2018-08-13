3797e9fe9fac19e549e3b064ff19a611b2f2c62e
DESKTOP-G1DGSJ4\Guopin <Kingdompin@163.com>
2018-08-13 20:46:24 +0800

    Build e3-item-web module.

---
+++ e3-item-web/src/main/java/guo/ping/e3mall/item/ItemWebApplicationStarter.java
@@ -0,0 +1,11 @@
+package guo.ping.e3mall.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemWebApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemWebApplicationStarter.class, args);
+    }
+}
